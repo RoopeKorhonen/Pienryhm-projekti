e5334eb4a6febe68d5b90a72e014486da6f8daf5
Roopekorhonen pöytäkone <roope.rudolf.korhonen@gmail.com>
2022-09-27 09:45:28 +0300

    Kysely funktio pitäisi toimia

--- Roope kansio/Roope file.py
+++ Roope kansio/Roope file.py
@@ -15,6 +15,21 @@ connection = mysql.connector.connect(
 
 
 def countryselecting():
-    country = str(input("Where do you wanna fly?"))
-    lookingcountry = "SELECT continent WHERE iso_country = '" + country + '" "
-    return country
+    country = input("Where do you wanna fly?")
+    lookingcontinent = "SELECT DISTINCT continent from "' + country + '" "
+    looking_continent_cursor = connection.cursor()
+    looking_continent_cursor.execute(lookingcontinent)
+    looking_continent_result = looking_continent_cursor.fetchall()
+
+    lookingcountry = "SELECT DISTINCT name  from country where continent = "' + looking_continent_result + '" "
+    looking_country_cursor = connection.cursor()
+    looking_country_cursor.execute(lookingcountry)
+    looking_country_result = looking_country_cursor.fetchall()
+
+    chosencountry = "SELECT DISTINCT name from "' + looking_country_result +  '" = "' + country + '" "
+    chosen_country_cursor = connection.cursor()
+    chosen_country_cursor.execute(chosencountry)
+    result = chosen_country_cursor.fetchall()
+    return result
+
+print(countryselecting())
