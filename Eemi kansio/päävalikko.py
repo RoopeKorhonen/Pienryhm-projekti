31584a15b7671e78398dea8cb6cf2327e9c6cedd
eeko1 <eemi.korhonen@metropolia.fi>
2022-10-03 13:17:44 +0300

    päävalikko eka commit

---
+++ Eemi kansio/päävalikko.py
@@ -0,0 +1,18 @@
+print("Hello this is the Flying game Ultimatum\nPlease take your time and enjoy the ride\n")
+mainmenu_int = input("Main menu\n1.Play\n2.Scores\n3.Guide\n4.Quit\n")
+
+if mainmenu_int == "1":
+    print("Roberto")
+
+elif mainmenu_int == "2":
+    print("Hello team")
+
+
+elif mainmenu_int == "3":
+
+
+elif mainmenu_int == "4":
+    print("You have quit the game.")
+
+else:
+    print("WTF BRO")
