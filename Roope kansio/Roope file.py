e673b1cf274c5bb06ed1c876f61301d30c7bc816
Roopekorhonen pöytäkone <roope.rudolf.korhonen@gmail.com>
2022-10-06 12:31:38 +0300

    Kysely funktio pitäisi toimia

--- Roope kansio/Roope file.py
+++ Roope kansio/Roope file.py
@@ -4,6 +4,7 @@
 import random
 
 import mysql.connector
+
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -16,20 +17,21 @@ connection = mysql.connector.connect(
 
 def countryselecting():
     country = input("Where do you wanna fly?")
-    lookingcontinent = "SELECT DISTINCT continent from "' + country + '" "
+    looking_continent = "SELECT DISTINCT continent from "' + country + '" "
     looking_continent_cursor = connection.cursor()
     looking_continent_cursor.execute(lookingcontinent)
     looking_continent_result = looking_continent_cursor.fetchall()
 
-    lookingcountry = "SELECT DISTINCT name  from country where continent = "' + looking_continent_result + '" "
+    looking_country = "SELECT DISTINCT name  from country where continent = "' + looking_continent_result + '" "
     looking_country_cursor = connection.cursor()
-    looking_country_cursor.execute(lookingcountry)
+    looking_country_cursor.execute(looking_country)
     looking_country_result = looking_country_cursor.fetchall()
 
-    chosencountry = "SELECT DISTINCT name from "' + looking_country_result +  '" = "' + country + '" "
+    chosen_country = "SELECT DISTINCT name from "' + looking_country_result +  '" = "' + country + '" "
     chosen_country_cursor = connection.cursor()
-    chosen_country_cursor.execute(chosencountry)
+    chosen_country_cursor.execute(chosen_country)
     result = chosen_country_cursor.fetchall()
     return result
 
 print(countryselecting())
+
