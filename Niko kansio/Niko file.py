185956b6294aadba72940c8c0831308a7cbaf9ae
Niko Tossavainen <nikotossavainen1999@gmail.com>
2022-10-07 13:40:53 +0300

    niko

--- Niko kansio/Niko file.py
+++ Niko kansio/Niko file.py
@@ -8,7 +8,18 @@ connection = mysql.connector.connect(
          user='root',
          password='',
          autocommit=True
-         )
 
-co2_budget =
+def game_over():
+    print("Game over you ran out of co2 to consume")
+
+def co2_budget_tracker():
+    if co2_used < 15000:
+        co2_budget = (co2_budget - co2_used)
+        chooseCountry()
+    else:
+        game_over()
+
+def game_over():
+    print("Game over you ran out of co2 to consume")
+
 
