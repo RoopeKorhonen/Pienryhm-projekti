5e0098c5fb75c8f48ce7988428d80c67c6eee00d
onnif <onni.forsell@gmail.com>
2022-10-09 18:16:22 +0300

    Pelin voi nyt pelata monta kertaa samassa istunnossa

--- Onni kansio/testi.py
+++ Onni kansio/testi.py
@@ -20,7 +20,7 @@ def co2_tracker(value):
     global co2_budget
     co2_budget = co2_budget - value
     if co2_budget > 1000:
-        print(co2_budget)
+        print(f"You have {co2_budget:0.2f} of Co2 left")
         return
     else:
         game_over()
@@ -85,11 +85,33 @@ def chooseCountry():
 
 
 def game_over():
+    global username
     highScore_str = str(high_score_calculator())
     name_and_scoreADD = "INSERT INTO game (screen_name, highscores) values ('" + username + "', '" + highScore_str + "');"
     cursor_func(name_and_scoreADD)
     print(f"Game over, you have ran out of Co2 to consume. Your total highscore is {highScore_str}.")
-    exit()
+    choice = input("Main menu\n1.Play\n2.Scores\n3.Quit\n: ")
+    choice = choice.lower()
+
+    while choice != "1" or "2" or "3":
+        if choice == "1":
+            global airport_list
+            airport_list = visited_airport_list()
+            username = username_input()
+            global co2_budget
+            co2_budget = co2_budgetgiver()
+            global multiplier
+            multiplier = Difficulty()
+            spawn_point()
+        elif choice == "2":
+            highscore_menu()
+        elif choice == "3":
+            print("You have quit the game. Farewell!")
+            exit()
+        else:
+            print("Invalid input")
+        choice = input("Main menu\n1.Play\n2.Scores\n3.Quit\n: ")
+        choice = choice.lower()
 
 
 def spawn_point():
@@ -104,6 +126,7 @@ def spawn_point():
         global current_location
         current_location = current_airport
     chooseCountry()
+
 def Difficulty():
     diff_level = input("Please choose a difficulty level: \nThe Difficulty levels are Easy, Normal and Hard\n:")
     diff_level = diff_level.lower()
@@ -135,7 +158,7 @@ def cursor_func(sql_komento):                      #SQL PISTEYTYKSEN KOMENTO.
         port=3306,
         database='flight_game',
         user='root',
-        password='SeOnSiina!?',
+        password='gutpo80',
         autocommit=True
     )
     cursor = yhteys.cursor()
@@ -158,15 +181,21 @@ def visited_airport_list():
     visited_airports = []
     return visited_airports
 
+def highscore_menu():
+    print("Highscores menu.")
+    score_database = "SELECT screen_name, highscores FROM game ORDER BY highscores DESC LIMIT 5;"
+    cursor = connection.cursor()
+    cursor.execute(score_database)
+    result = cursor.fetchall()
+    for x in result:
+        print(x[0], x[1])
 
 main_menu_int = input("Main menu\n1.Play\n2.Scores\n3.Quit\n: ")
-if main_menu_int == "3":
-        print("You have quit the game. Farewell!")
-        exit()
-while main_menu_int != 1 or 2 or 3:
-    main_menu_int = input("Main menu\n1.Play\n2.Scores\n3.Quit\n: ")
+while main_menu_int != "1" or "2" or "3":
     if main_menu_int == "1":
+        global airport_list
         airport_list = visited_airport_list()
+        global username
         username = username_input()
         co2_budget = co2_budgetgiver()
         multiplier = Difficulty()
@@ -178,7 +207,9 @@ while main_menu_int != 1 or 2 or 3:
             cursor.execute(score_database)
             result = cursor.fetchall()
             for x in result:
-                print(x[0],x[1])
+                print(x[0], x[1])
     elif main_menu_int == "3":
         print("You have quit the game. Farewell!")
         exit()
+    else:
+        main_menu_int = input("Main menu\n1.Play\n2.Scores\n3.Quit\n: ")
